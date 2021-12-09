a66291a1a37f3203c9ac117b0bbbbc0c7c1ffc12
James Brooks <jamescbrooks2020@gmail.com>
2021-12-09 23:12:24 +1100

    Setting up automatic dates for calendar.

---
+++ src/client/js/getLocationInput.js
@@ -0,0 +1,16 @@
+const getUserInput = () => {
+    const countryInput = document.getElementById("country").value
+    const cityInput = document.getElementById("city").value
+    if (countryInput === "Select a Country" || cityInput === "") {
+        alert("Please Select a Country and City");
+        if (countryInput === "Select a Country") {
+            
+
+        }
+
+    } else {
+        if ()
+
+    }
+
+}
