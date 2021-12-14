d247262d03e90a15e326949f249f264695c0e7fe
James Brooks <jamescbrooks2020@gmail.com>
2021-12-14 12:00:15 +1100

    Created logic for icon selection.

---
+++ src/client/js/iconTest.js
@@ -0,0 +1,194 @@
+const iconSelection = (code) => {
+    if (code === 200) {
+        const iconD = "t01d"
+        const iconN = "t01n"
+    }
+
+    if (code === 201) {
+        const iconD = "t02d"
+        const iconN = "t02n"
+    }
+
+    if (code === 202) {
+        const iconD = "t03d"
+        const iconN = "t03n"
+    }
+
+    if (code === 230) {
+        const iconD = "t04d"
+        const iconN = "t04n"
+    }
+
+    if (code === 231) {
+        const iconD = "t04d"
+        const iconN = "t04n"
+    }
+
+    if (code === 232) {
+        const iconD = "t04d"
+        const iconN = "t04n"
+    }
+
+    if (code === 233) {
+        const iconD = "t05d"
+        const iconN = "t05n"
+    }
+
+    if (code === 300) {
+        const iconD = "d01d"
+        const iconN = "d01n"
+    }
+
+    if (code === 301) {
+        const iconD = "d02d"
+        const iconN = "d02n"
+    }
+
+    if (code === 302) {
+        const iconD = "d03d"
+        const iconN = "d03n"
+    }
+
+    if (code === 500) {
+        const iconD = "r01d"
+        const iconN = "r01n"
+    }
+
+    if (code === 501) {
+        const iconD = "r02d"
+        const iconN = "r02n"
+    }
+
+    if (code === 502) {
+        const iconD = "r03d"
+        const iconN = "r03n"
+    }
+
+    if (code === 511) {
+        const iconD = "f01d"
+        const iconN = "f01n"
+    }
+
+    if (code === 520) {
+        const iconD = "r04d"
+        const iconN = "r04n"
+    }
+
+    if (code === 521) {
+        const iconD = "r05d"
+        const iconN = "r05n"
+    }
+
+    if (code === 522) {
+        const iconD = "r06d"
+        const iconN = "r06n"
+    }
+
+    if (code === 600) {
+        const iconD = "s01d"
+        const iconN = "s01n"
+    }
+
+    if (code === 601) {
+        const iconD = "s02d"
+        const iconN = "s02n"
+    }
+
+    if (code === 602) {
+        const iconD = "s03d"
+        const iconN = "s03n"
+    }
+
+    if (code === 610) {
+        const iconD = "s04d"
+        const iconN = "s04n"
+    }
+
+    if (code === 611) {
+        const iconD = "s05d"
+        const iconN = "s05n"
+    }
+
+    if (code === 612) {
+        const iconD = "s05d"
+        const iconN = "s05n"
+    }
+
+    if (code === 621) {
+        const iconD = "s01d"
+        const iconN = "s01n"
+    }
+
+    if (code === 622) {
+        const iconD = "s02d"
+        const iconN = "s02n"
+    }
+
+    if (code === 623) {
+        const iconD = "s06d"
+        const iconN = "s06n"
+    }
+
+    if (code === 700) {
+        const iconD = "a01d"
+        const iconN = "a01n"
+    }
+
+    if (code === 711) {
+        const iconD = "a02d"
+        const iconN = "a02n"
+    }
+
+    if (code === 721) {
+        const iconD = "a03d"
+        const iconN = "a03n"
+    }
+
+    if (code === 731) {
+        const iconD = "a04d"
+        const iconN = "a04n"
+    }
+
+    if (code === 741) {
+        const iconD = "a05d"
+        const iconN = "a05n"
+    }
+
+    if (code === 751) {
+        const iconD = "a06d"
+        const iconN = "a06n"
+    }
+
+    if (code === 800) {
+        const iconD = "c01d"
+        const iconN = "c01n"
+    }
+
+    if (code === 801) {
+        const iconD = "c02d"
+        const iconN = "c02n"
+    }
+
+    if (code === 802) {
+        const iconD = "c02d"
+        const iconN = "c02n"
+    }
+
+    if (code === 803) {
+        const iconD = "c03d"
+        const iconN = "c03n"
+    }
+
+    if (code === 804) {
+        const iconD = "c04d"
+        const iconN = "c04n"
+    }
+
+    if (code === 900) {
+        const iconD = "u00d"
+        const iconN = "u00n"
+    }
+
+    return {day: iconD, night: iconN};
+
+}
