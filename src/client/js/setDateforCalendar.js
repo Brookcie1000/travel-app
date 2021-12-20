82b1ac70a800f0a2780271583f11c6bf4f752a8c
James Brooks <jamescbrooks2020@gmail.com>
2021-12-20 12:24:09 +1100

    Dates now update correctly for new month or year.

--- src/client/js/setDateforCalendar.js
+++ src/client/js/setDateforCalendar.js
@@ -14,19 +14,58 @@ const getTodayDate = () => {
 
     }
 
-    updateHTML(year, month, day);
+    return {one: day, two: month, three: year}
 
 }
 
 const updateHTML = (year, month, day) => {
+    const date = new Date();
+    let maxDay = day + 15;
+    let maxMonth = date.getMonth() + 1;
+    let maxYear;
     const htmlElement = document.getElementById("date-start")
     htmlElement.min = `${year}-${month}-${day}`
-    htmlElement.max = `${year}-${month}-${day+15}` //set the max date to 16 day forecast the API supplies.
+    if ((month === "01") || (month === "03") || (month === "05") || (month === "07" || (month === "08") || (month === 10) || (month === 12))) {
+        if (maxDay > 31) {
+            maxDay = (day + 15) - 31;
+            maxDay = "0" + maxDay;
+            maxMonth = maxMonth + 1;
+
+            if (maxMonth < 10) {
+                maxMonth = "0" + maxMonth;
+
+            }
+
+            if (maxMonth > 12) {
+                maxMonth = "01";
+                maxYear = year + 1;
+
+            }
+            
+        }
+
+    } else {
+        if (maxDay > 30) {
+            maxDay = (day + 15) - 30;
+            maxMonth = maxMonth + 1;
+
+            console.log(maxMonth)
+            if (maxMonth < 10) {
+                maxMonth = "0" + max;
+
+            }
+
+        }
+
+    }
+    htmlElement.max = `${maxYear}-${maxMonth}-${maxDay}` //set the max date to 16 day forecast the API supplies.
     htmlElement.value = `${year}-${month}-${day}`
 
 }
 
-getTodayDate();
+const currentDate = getTodayDate(); //store the date
+
+updateHTML(currentDate.three, currentDate.two, currentDate.one); //update the date on page
 
 export {getTodayDate}
 export {updateHTML}
